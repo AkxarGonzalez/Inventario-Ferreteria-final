b09fba5c8133bbbaed7e7e256e866b6155db0d07
akxargonzalez13@gmail.com <akxargonzalez13@gmail.com>
2024-08-12 23:09:18 -0600

    primer commint

--- UserModuleForm.Designer.cs
+++ UserModuleForm.Designer.cs
@@ -30,11 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserModuleForm));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxCerrar = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.txtNombreCompleto = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.txtContraseña = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.txtPuesto = new System.Windows.Forms.TextBox();
@@ -42,14 +41,17 @@
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnActualizar = new System.Windows.Forms.Button();
             this.btnLimpiar = new System.Windows.Forms.Button();
-            this.pictureBoxCerrar = new System.Windows.Forms.PictureBox();
+            this.txtReContraseña = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombreCompleto = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCerrar)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel1.BackColor = System.Drawing.Color.SeaGreen;
             this.panel1.Controls.Add(this.pictureBoxCerrar);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
@@ -58,14 +60,25 @@
             this.panel1.Size = new System.Drawing.Size(780, 52);
             this.panel1.TabIndex = 0;
             // 
+            // pictureBoxCerrar
+            // 
+            this.pictureBoxCerrar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxCerrar.Image")));
+            this.pictureBoxCerrar.Location = new System.Drawing.Point(726, 3);
+            this.pictureBoxCerrar.Name = "pictureBoxCerrar";
+            this.pictureBoxCerrar.Size = new System.Drawing.Size(51, 46);
+            this.pictureBoxCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCerrar.TabIndex = 11;
+            this.pictureBoxCerrar.TabStop = false;
+            this.pictureBoxCerrar.Click += new System.EventHandler(this.pictureBoxCerrar_Click);
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Hoefler Text Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(3, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 24);
+            this.label1.Size = new System.Drawing.Size(170, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Modulo de Usuario";
             // 
@@ -73,7 +86,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Location = new System.Drawing.Point(12, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 20);
             this.label2.TabIndex = 1;
@@ -81,40 +94,25 @@
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(176, 125);
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(171, 92);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(549, 20);
+            this.txtUsuario.Size = new System.Drawing.Size(549, 26);
             this.txtUsuario.TabIndex = 2;
-            // 
-            // txtNombreCompleto
-            // 
-            this.txtNombreCompleto.Location = new System.Drawing.Point(176, 165);
-            this.txtNombreCompleto.Name = "txtNombreCompleto";
-            this.txtNombreCompleto.Size = new System.Drawing.Size(549, 20);
-            this.txtNombreCompleto.TabIndex = 4;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 165);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Nombre Completo :";
             // 
             // txtContraseña
             // 
-            this.txtContraseña.Location = new System.Drawing.Point(176, 210);
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.Location = new System.Drawing.Point(176, 184);
             this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.Size = new System.Drawing.Size(549, 20);
+            this.txtContraseña.Size = new System.Drawing.Size(549, 26);
             this.txtContraseña.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 210);
+            this.label4.Location = new System.Drawing.Point(12, 184);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 5;
@@ -122,16 +120,17 @@
             // 
             // txtPuesto
             // 
-            this.txtPuesto.Location = new System.Drawing.Point(176, 258);
+            this.txtPuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPuesto.Location = new System.Drawing.Point(176, 266);
             this.txtPuesto.Name = "txtPuesto";
-            this.txtPuesto.Size = new System.Drawing.Size(549, 20);
+            this.txtPuesto.Size = new System.Drawing.Size(549, 26);
             this.txtPuesto.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 256);
+            this.label5.Location = new System.Drawing.Point(12, 269);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(67, 20);
             this.label5.TabIndex = 7;
@@ -143,7 +142,7 @@
             this.btnGuardar.FlatAppearance.BorderSize = 0;
             this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.Location = new System.Drawing.Point(154, 323);
+            this.btnGuardar.Location = new System.Drawing.Point(147, 364);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(164, 49);
             this.btnGuardar.TabIndex = 9;
@@ -157,12 +156,13 @@
             this.btnActualizar.FlatAppearance.BorderSize = 0;
             this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnActualizar.Location = new System.Drawing.Point(361, 323);
+            this.btnActualizar.Location = new System.Drawing.Point(354, 364);
             this.btnActualizar.Name = "btnActualizar";
             this.btnActualizar.Size = new System.Drawing.Size(164, 49);
             this.btnActualizar.TabIndex = 10;
             this.btnActualizar.Text = "Actualizar";
             this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // btnLimpiar
             // 
@@ -170,7 +170,7 @@
             this.btnLimpiar.FlatAppearance.BorderSize = 0;
             this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpiar.Location = new System.Drawing.Point(563, 323);
+            this.btnLimpiar.Location = new System.Drawing.Point(556, 364);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(164, 49);
             this.btnLimpiar.TabIndex = 11;
@@ -178,22 +178,49 @@
             this.btnLimpiar.UseVisualStyleBackColor = false;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
-            // pictureBoxCerrar
+            // txtReContraseña
             // 
-            this.pictureBoxCerrar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxCerrar.Image")));
-            this.pictureBoxCerrar.Location = new System.Drawing.Point(752, 3);
-            this.pictureBoxCerrar.Name = "pictureBoxCerrar";
-            this.pictureBoxCerrar.Size = new System.Drawing.Size(25, 22);
-            this.pictureBoxCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxCerrar.TabIndex = 11;
-            this.pictureBoxCerrar.TabStop = false;
-            this.pictureBoxCerrar.Click += new System.EventHandler(this.pictureBoxCerrar_Click);
+            this.txtReContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReContraseña.Location = new System.Drawing.Point(214, 227);
+            this.txtReContraseña.Name = "txtReContraseña";
+            this.txtReContraseña.Size = new System.Drawing.Size(506, 26);
+            this.txtReContraseña.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(215, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Volver a escribir Contraseña :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre Completo :";
+            // 
+            // txtNombreCompleto
+            // 
+            this.txtNombreCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreCompleto.Location = new System.Drawing.Point(171, 143);
+            this.txtNombreCompleto.Name = "txtNombreCompleto";
+            this.txtNombreCompleto.Size = new System.Drawing.Size(549, 26);
+            this.txtNombreCompleto.TabIndex = 4;
             // 
             // UserModuleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(780, 405);
+            this.ClientSize = new System.Drawing.Size(780, 425);
+            this.Controls.Add(this.txtReContraseña);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.btnActualizar);
             this.Controls.Add(this.btnGuardar);
@@ -223,16 +250,18 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtUsuario;
-        private System.Windows.Forms.TextBox txtNombreCompleto;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtContraseña;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtPuesto;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button btnGuardar;
-        private System.Windows.Forms.Button btnActualizar;
-        private System.Windows.Forms.Button btnLimpiar;
         private System.Windows.Forms.PictureBox pictureBoxCerrar;
+        public System.Windows.Forms.Button btnGuardar;
+        public System.Windows.Forms.Button btnActualizar;
+        public System.Windows.Forms.Button btnLimpiar;
+        public System.Windows.Forms.TextBox txtUsuario;
+        public System.Windows.Forms.TextBox txtContraseña;
+        public System.Windows.Forms.TextBox txtPuesto;
+        public System.Windows.Forms.TextBox txtReContraseña;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox txtNombreCompleto;
     }
 }
--- UserModuleForm.cs
+++ UserModuleForm.cs
@@ -13,7 +13,7 @@ namespace Inventario_Ferreteria
 {
     public partial class UserModuleForm : Form
     {
-        SqlConnection conex = new SqlConnection(@"Data Source=DESKTOP-LH5HOO2\SQLEXPRESS;Initial Catalog=bd_ferreteria;Integrated Security=True");
+        SqlConnection conex = new SqlConnection(@"Data Source=SISTEMAS\SQLEXPRESS;Initial Catalog=bd_ferreteria;Integrated Security=True");
         SqlCommand cn = new SqlCommand();
 
       public UserModuleForm()
@@ -31,7 +31,7 @@ namespace Inventario_Ferreteria
 
             try
             {
-                if (txtContraseña.Text != txtContraseña.Text)
+                if (txtContraseña.Text != txtReContraseña.Text)
                 {
                     MessageBox.Show("La Contraseña no coincide","Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
@@ -43,6 +43,7 @@ namespace Inventario_Ferreteria
                     cn.Parameters.AddWithValue("@NombreCompleto", txtNombreCompleto.Text);
                     cn.Parameters.AddWithValue("@Contrasena", txtContraseña.Text);
                     cn.Parameters.AddWithValue("@Puesto", txtPuesto.Text);
+                    
                     conex.Open();
                     cn.ExecuteNonQuery();
                     conex.Close();
@@ -69,7 +70,7 @@ namespace Inventario_Ferreteria
             txtUsuario.Clear();
             txtNombreCompleto.Clear();
             txtContraseña.Clear();
-            txtContraseña.Clear();
+            txtReContraseña.Clear();
             txtPuesto.Clear();
         }
 
@@ -77,29 +78,32 @@ namespace Inventario_Ferreteria
         {
             try
             {
-                if (txtContraseña.Text != txtContraseña.Text)
+                if (txtContraseña.Text != txtReContraseña.Text)
                 {
-                    MessageBox.Show("La Contraseña no coincide", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("LA CONTRASEÑA NO ES CORRECTA", "ADVERTENCIA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
-                if (MessageBox.Show("Estas seguro de actualizar este usuario?", "Actualizar Registro", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                if (MessageBox.Show("¿Está seguro de que desea actualizar este usuario?", "Actualizar Registro" , MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
                 {
-                    cn = new SqlCommand("UPDATE Usuarios SET Nombre_Completo = @NombreCompleto,Contrasena=@Contrasena,Puesto=@Puesto WHERE Usuario LIKE '"+txtUsuario.Text +"' ", conex);
-                    cn.Parameters.AddWithValue("@NombreCompleto", txtNombreCompleto.Text);
+                    cn = new SqlCommand("UPDATE USUARIOS SET Nombre_Completo = @Nombre_Completo, Contrasena=@Contrasena, Puesto=@puesto WHERE usuario LIKE '" + txtUsuario.Text + "' ", conex);
+                    cn.Parameters.AddWithValue("@Nombre_Completo", txtNombreCompleto.Text);
                     cn.Parameters.AddWithValue("@Contrasena", txtContraseña.Text);
                     cn.Parameters.AddWithValue("@Puesto", txtPuesto.Text);
                     conex.Open();
                     cn.ExecuteNonQuery();
                     conex.Close();
-                    MessageBox.Show("El usuario ha sido actualizado exitosamente!!");
+                    MessageBox.Show("El Usuario ha sido actualizado!");
                     this.Dispose();
+
                 }
+                
 
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+
         }
     }
 }
